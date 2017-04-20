e8309a56d116a39926aceaba7bdaf1ebe207ca81
RoadshowExecution <RoadshowExecution@gmail.com>
2017-04-20 09:40:35 -0400

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -337,8 +337,8 @@
               <br />\
               " + '<b>&#8226; ADDRESS: </b>' + data[row][2] + "\
               <br />\
-              " + '<b>&#8226; Distance: </b>' + getDistance(results[0].geometry.location.lat(), results[0].geometry.location.lng(), data[row][3], data[row][4]) + "\
-              <br />\
+             
+              
                 </div>\
             </div>\
           </div>";
